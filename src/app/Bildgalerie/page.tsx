222433e67f1592bf384e73a21ade20c37692be52
Marlin Jai <marlinjp@icloud.com>
2024-02-15 20:05:17 +0100

    changed image gallery

--- src/app/Bildgalerie/page.tsx
+++ src/app/Bildgalerie/page.tsx
@@ -3,6 +3,7 @@ import SiteLayout from "../_components/UtilityComponents/SiteLayout";
 import Zoomgallery from "../_components/Swiper&GaleryComponents/Zoomgallery";
 import { Gallery } from "@/types/sanityTypes";
 import { zoomGalleryQuery } from "@/sanity/lib/sanity.queries";
+import LightImageGallery from "../_components/Swiper&GaleryComponents/LightImageGallery";
 
 export default async function Bildgalerie() {
   const galleryData = await sanityFetch<Gallery[]>({ query: zoomGalleryQuery });
@@ -14,13 +15,17 @@ export default async function Bildgalerie() {
   // Assume the first gallery in the array; you can modify this
   const firstGallery = galleryData[0];
 
+  console.log(firstGallery.images);
+
   return (
     <SiteLayout>
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-4xl mt-10 text-brand-colour-light">Bildgalerie</h1>
       </div>
-      <div className="p-4 md:p-12 xl:p-20 flex justify-center">
-        <Zoomgallery images={firstGallery.images}></Zoomgallery>
+      <div className="p-4 md:p-12 xl:p-20 flex flex-wrap justify-center">
+        {/* <Zoomgallery images={firstGallery.images}></Zoomgallery> */}
+
+        <LightImageGallery images={firstGallery.images}></LightImageGallery>
       </div>
     </SiteLayout>
   );
---
+++ src/app/_components/Swiper&GaleryComponents/LightImageGallery.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import React from "react";
+import LightGallery from "lightgallery/react";
+
+// import styles
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
+
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
+import { GalleryImage } from "@/types/sanityTypes";
+import { urlForImage } from "@/sanity/lib/sanity.image";
+import { client } from "@/sanity/lib/sanity.client";
+import Image from "next/image";
+
+const builder = urlForImage(client);
+
+interface ZoomgalleryProps {
+  images: GalleryImage[];
+}
+export default function Gallery(props: ZoomgalleryProps) {
+  console.log(props);
+
+  const onInit = () => {
+    console.log("lightGallery has been initialized");
+  };
+  return (
+    <div className="App columns-2 gap-0 lg:columns-3 xl:columns-4 [&>img:not(:first-child)]:mt-5 lg:[&>img:not(:first-child)]:mt-8">
+      <LightGallery
+        onInit={onInit}
+        speed={500}
+        plugins={[lgZoom, lgThumbnail]}
+        elementClassNames="my-gallery"
+      >
+        {props.images.map((image, index) => {
+          return (
+            <a href={builder.image(image).url()} key={index}>
+              <Image
+                alt={image.alt}
+                src={builder.image(image).url()}
+                sizes="(max-width: 1200px) 500px, 800px"
+                width={800}
+                height={600}
+                key={index}
+                className="object-cover"
+              />
+            </a>
+          );
+        })}
+      </LightGallery>
+    </div>
+  );
+}
