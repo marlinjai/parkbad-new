61dbe60404b262f4fc649a0004f50a82186f04df
Marlin Jai <marlinjp@icloud.com>
2024-04-04 01:19:56 +0200

    speed improvements

--- src/app/_components/Homepage_Components/videoSection.tsx
+++ src/app/_components/Homepage_Components/videoSection.tsx
@@ -8,6 +8,7 @@ export default function VideoSection() {
         src={"/Header_BG_II_big_webm_bigger.mp4"}
         typeof="video/mp4"
         className="left-0 top-0 max-h-vh90 md:max-h-vh100 min-h-vidbg w-vw100 object-cover"
+        preload="auto"
         playsInline
         autoPlay
         loop
