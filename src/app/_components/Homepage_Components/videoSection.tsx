ea6b82afbfa3ae2891c1e7dc1804774ee807f680
Marlin Jai <marlinjp@icloud.com>
2024-04-30 20:09:03 +0200

    changed video

--- src/app/_components/Homepage_Components/videoSection.tsx
+++ src/app/_components/Homepage_Components/videoSection.tsx
@@ -6,7 +6,7 @@ export default function VideoSection() {
     <div className="video-BG  relative  flex min-h-vidbg flex-col  content-center justify-between">
       {/* Video background */}
       <video
-        src={"/Header_BG_II_big_webm_bigger.mp4"}
+        src="https://www.parkbad-gt.de/Header_BG_II_big_webm_bigger.mp4"
         typeof="video/mp4"
         className="left-0 top-0 max-h-vh90 md:max-h-vh100 min-h-vidbg w-vw100 object-cover"
         preload="auto"
